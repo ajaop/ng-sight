69a690ed8b0d6bdac20673863c9e203ed9d52c8d
ajaop <67429233+ajaop@users.noreply.github.com>
2021-03-03 11:29:47 +0100

    Create server component

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../shared/server';
+
+const SAMPLE_SERVERS = [
+  {id: 1, name: 'dev-web', isOnline: true},
+  {id: 2, name: 'dev-mail', isOnline: false},
+  {id: 3, name: 'prod-web', isOnline: true},
+  {id: 4, name: 'prod-mail', isOnline: true}
+];
 
 @Component({
   selector: 'app-section-health',
@@ -7,7 +15,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionHealthComponent implements OnInit {
 
-  constructor() { }
+  //constructor() { }
+
+  servers: Server[] = SAMPLE_SERVERS;
 
   ngOnInit(): void {
   }
